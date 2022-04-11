29a645fafe350b14162098f499a6c232f898c353
takashi <yagita.takashi@gmail.com>
2022-04-11 17:00:38 +0900

    first commit

--- tests/unit/test_eks_pipeline_for_container_build_stack.py
+++ tests/unit/test_eks_pipeline_for_container_build_stack.py
@@ -1,10 +1,10 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from eks_pipeline_for_container_build.eks_pipeline_for_container_build_stack import EksPipelineForContainerBuildStack
+from _stacks.pipeline import EksPipelineForContainerBuildStack
 
 # example tests. To run these tests, uncomment this file along with the example
-# resource in eks_pipeline_for_container_build/eks_pipeline_for_container_build_stack.py
+# resource in _stacks/pipeline.py
 def test_sqs_queue_created():
     app = core.App()
     stack = EksPipelineForContainerBuildStack(app, "eks-pipeline-for-container-build")
