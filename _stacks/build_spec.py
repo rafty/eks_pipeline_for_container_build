7d9eb44c86fcee330939c0d58dd17ee68f553e6a
takashi <yagita.takashi@gmail.com>
2022-04-12 13:07:06 +0900

    first commit

---
+++ _stacks/build_spec.py
@@ -0,0 +1,43 @@
+build_spec_object = {
+    "version": "0.2",
+
+    "phases": {
+        "install": {
+            "runtime-versions": {
+                    "python": 3.8,
+                    "docker": 19
+            },
+        },
+
+        "pre_build": {
+            "commands": [
+                    'echo --- Logging in to Amazon ECR ---',
+                    'echo $AWS_DEFAULT_REGION',
+                    'echo $AWS_ACCOUNT_ID',
+                    'aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com',
+                    'echo --- Logging in to DockerHub ---',
+                    'DOCKERHUB_USER_ID=$(aws --region="$AWS_DEFAULT_REGION" ssm get-parameters --names "/CodeBuild/DOCKERHUB_USER_ID" | jq --raw-output ".Parameters[0].Value")',
+                    'DOCKERHUB_PASSWORD=$(aws --region="$AWS_DEFAULT_REGION" ssm get-parameters --names "/CodeBuild/DOCKERHUB_PASSWORD" --with-decryption | jq --raw-output ".Parameters[0].Value")',
+                    'echo $DOCKERHUB_PASSWORD | docker login -u $DOCKERHUB_USER_ID --password-stdin',
+            ],
+        },
+
+        "build": {
+            "commands": [
+                    'echo --- Building Docker image ---',
+                    'COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-8)',
+                    'IMAGE_TAG=$(date +%Y-%m-%dH%H.%M.%S)-${COMMIT_HASH:=latest}',
+                    'docker build --no-cache -t $CONTAINER_IMAGE_NAME .',
+                    'docker tag $CONTAINER_IMAGE_NAME:latest $CONTAINER_IMAGE_NAME:$IMAGE_TAG',
+            ],
+        },
+
+        "post_build": {
+            "commands": [
+                    'echo --- Pushing the Docker images ---',
+                    'docker push $CONTAINER_IMAGE_NAME:$IMAGE_TAG',
+                    'echo --- Build completed ---',
+            ],
+        },
+    },
+}
--- _stacks/pipeline.py
+++ _stacks/pipeline.py
@@ -1,10 +1,189 @@
+import aws_cdk
 from aws_cdk import Stack
 from constructs import Construct
+from aws_cdk import aws_iam
+from aws_cdk.pipelines import CodePipeline
+from aws_cdk.pipelines import CodePipelineSource
+from aws_cdk.pipelines import ShellStep
+from aws_cdk.pipelines import CodeBuildStep
+from aws_cdk import aws_codebuild
+from aws_cdk import aws_ecr
+from constructs import Construct
+from _stacks.build_spec import build_spec_object
 
 
 class PipelineStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, env: aws_cdk.Environment, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        repository_name = self.node.try_get_context('github_repository_name')
+        github_connection_arn = self.node.try_get_context('github_connection_arn')
+
+        github_connection = CodePipelineSource.connection(
+            repo_string=repository_name,
+            branch='master',
+            connection_arn=github_connection_arn
+        )
+
+        ecr_pipeline = CodePipeline(
+            scope=self,
+            id='EcrPipeline',
+            pipeline_name='ecr_pipeline',
+            self_mutation=False,
+            synth=ShellStep(
+                id='Synth',
+                input=github_connection,
+                commands=[
+                    'npm install -g aws-cdk',
+                    'python -m pip install -r requirements.txt',
+                    'cdk synth'
+                ]
+            )
+        )
+
+        # ---------------------------------------------------------
+        #  Stage 追加
+        #  ECR Repository作成 と Docker Build & Push
+        # ---------------------------------------------------------
+
+        # ecr_repository_name = self.node.try_get_context('ecr_repository_name')
+        #
+        # ecr_repo = aws_ecr.Repository(
+        #     self,
+        #     'SamplePythonAppRepo',
+        #     repository_name=ecr_repository_name,
+        #     image_scan_on_push=True,  # Image Scan
+        #     # removal_policy=aws_cdk.RemovalPolicy.DESTROY, # stack削除時の動作
+        #     # lifecycle_rules=[removal_old_image]  # imageの世代管理
+        # )
+
+        # ---------------------------------------------------------
+        #  Stage にはStackとStepを追加する
+        #  StackはCDK Stackのことである。
+        #  StageにCDK Stackを入れる
+        # ---------------------------------------------------------
+
+        # ----------------------------------------
+        # Stage - Policy
+        # ----------------------------------------
+        ecr_policy = aws_iam.PolicyStatement(
+            actions=[
+                # 'codebuild:*',
+                'ecr:PutImage',
+                'ecr:BatchCheckLayerAvailability',
+                'ecr:CompleteLayerUpload',
+                'ecr:InitiateLayerUpload',
+                'ecr:UploadLayerPart',
+                'ecr:GetAuthorizationToken'
+            ],
+            effect=aws_iam.Effect.ALLOW,
+            resources=['*']
+        )
+        ssm_policy = aws_iam.PolicyStatement(
+            effect=aws_iam.Effect.ALLOW,
+            actions=['ssm:GetParameter', 'ssm:GetParameters'],
+            resources=[f'arn:aws:ssm:{self.region}:{self.account}:parameter/*']
+        )
+        logs_policy = aws_iam.PolicyStatement(
+            actions=['logs:GetLogEvents'],
+            effect=aws_iam.Effect.ALLOW,
+            resources=[f'arn:aws:logs:{self.region}:{self.account}:log-group:/aws/codebuild/*:*']
+        )
+        # ----------------------------------------
+
+        ecr_repo_stage = EcrRepositoryStage(
+            scope=self,
+            construct_id='SampleAppRcrRepoStage',
+            env=env
+        )
+
+        build_spec = aws_codebuild.BuildSpec.from_object(build_spec_object)
+        ecr_repository_name = self.node.try_get_context('ecr_repository_name')
+        build_container_project = CodeBuildStep(
+            id='DockerBuildStep',
+            build_environment=aws_codebuild.BuildEnvironment(
+                                build_image=aws_codebuild.LinuxBuildImage.STANDARD_5_0,
+                                privileged=True),  # for docker build
+            input=github_connection,
+            partial_build_spec=build_spec,
+            commands=[],  # need empty by partial_build_spec,
+            role_policy_statements=[
+                ecr_policy,
+                logs_policy,
+                ssm_policy
+            ],
+            env={
+                'AWS_ACCOUNT_ID': self.account,
+                'CONTAINER_IMAGE_NAME': ecr_repository_name,
+            }
+        )
+
+        ecr_pipeline.add_stage(
+            stage=ecr_repo_stage,
+            post=[build_container_project],
+        )
+
+        # ecr_repo_actions = ["ecr:PutImage",
+        #                     "ecr:BatchCheckLayerAvailability",
+        #                     "ecr:CompleteLayerUpload",
+        #                     "ecr:InitiateLayerUpload",
+        #                     "ecr:UploadLayerPart"]
+        #
+        # for perm in ecr_repo_actions:
+        #     ecr_repo.grant(buildContainerProject, perm)
+        #
+        # _iam.Grant.add_to_principal(
+        #     actions=["ecr:GetAuthorizationToken"],
+        #     resource_arns=["*"],
+        #     grantee=buildContainerProject
+        # )
+
+
+class EcrRepositoryStage(aws_cdk.Stage):
+    # ---------------------------------------------------------
+    #  Stage にはStackとStepを追加する
+    #  StackはCDK Stackのことである。
+    #  StageにCDK Stackを入れる
+    # ---------------------------------------------------------
+    def __init__(self,
+                 scope: Construct,
+                 construct_id: str,
+                 env: aws_cdk.Environment,
+                 **kwargs):
+        super().__init__(scope, construct_id, **kwargs)
+
+        self.__ecr_repo_stack = EcrRepositoryStack(
+            scope=self,
+            construct_id=construct_id,
+            env=env
+        )
+
+    @property
+    def ecr_repo_stack(self):
+        return self.__ecr_repo_stack
+
+
+class EcrRepositoryStack(aws_cdk.Stack):
+    # ---------------------------------------------------------
+    #  Stageに追加するStack
+    #  StackはCDK Stackのことである。
+    # ---------------------------------------------------------
+    def __init__(self,
+                 scope: Construct,
+                 construct_id: str,
+                 env: aws_cdk.Environment,
+                 **kwargs) -> None:
+
+        super().__init__(scope, construct_id, **kwargs)
+
+        ecr_repository_name = self.node.try_get_context('ecr_repository_name')
+        aws_ecr.Repository(
+            scope=self,
+            id=f'{ecr_repository_name}-Stack',
+            repository_name=ecr_repository_name,
+            image_scan_on_push=True,  # Image Scan
+            # removal_policy=aws_cdk.RemovalPolicy.DESTROY, # stack削除時の動作
+            # lifecycle_rules=[removal_old_image]  # imageの世代管理
+        )
 
